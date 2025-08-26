df4b83ac44e833ee79552901b3a828d13f805d8f
vijayragavan <vijay.ragavan@sita.aero>
2025-08-26 15:59:22 +0100

    First Commit

--- backend.tf
+++ backend.tf
@@ -1,10 +1,6 @@
 terraform {
   backend "azurerm" {
-    # tenant_id = "2e119e68-e37d-48ba-85b4-0691c2c1ab19"
-    # client_id = "74ff0f76-2b22-4a02-b318-472316ef730f"
-    # client_secret = "YqM8Q~VkZ6Z~VcnjsIKXlmKYT8x3Va.o~_sDIcCY"
-    resource_group_name = "dev-test"
-    # subscription_id = "610c3c5e-6ab7-4e4d-a170-b0e9ad4f72ff"
+        resource_group_name = "dev-test"
     storage_account_name = "statefule"
     container_name = "tfstate"
     key = "dev-test.tfstate"
